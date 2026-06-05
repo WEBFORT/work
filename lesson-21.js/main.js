4feca03c586c417d927cbc1ba929511a3d1ec8c6
sinegubkaila3-dev <sinegubkaila3@gmail.com>
2026-06-06 00:43:10 +0400

    Add files via upload

---
+++ lesson-21.js/main.js
@@ -0,0 +1,18 @@
+const firstName = `Меня зовут Alex`;
+const lastName = `Pack,`;
+const isStudent = false;
+const userAge = `мне 18 лет. Я ученик курса:`;
+const currentYear = 2026;
+const birthYear = 2008;
+const differ = currentYear - userAge;
+
+console.log(firstName, lastName, userAge, isStudent);
+
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+
+console.log(result)
