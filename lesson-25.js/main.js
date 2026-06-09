4ed2a7037f5e9da53a85fe4014740cf0cc1d6368
sinegubkaila3-dev <sinegubkaila3@gmail.com>
2026-06-09 05:47:06 +0400

    Add files via upload

---
+++ lesson-25.js/main.js
@@ -0,0 +1,40 @@
+// 1 задача
+
+// const user = {
+//     firstName: "Alex",
+//     lastName: "Pack",
+//     age: 18,
+//     inPrograms: false
+// }
+// console.log(user)
+
+// 2 задача
+
+// const isEmpty = (obj) => {
+// 	for (const _ in obj) {
+// 		return false;
+// 	}
+// 	return true;
+// };
+
+// const test = { test: 123 };
+
+// console.log(isEmpty(test));
+
+// 3 задача
+
+// -----------------------------------------
+
+// 4 задача
+
+const myObject = {
+	method1() {
+		console.log("Метод 1 вызван");
+	},
+	method2() {
+		console.log("Метод 2 вызван");
+	},
+	property: "Это не метод",
+};
+
+console.log(myObject)
